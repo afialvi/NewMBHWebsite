f040c5890a25beb90a140806585debdd19cbf6cf
Farman Ahmed <farmanahmed2007@gmail.com>
2020-05-12 04:25:30 +0500

    Resolving CSS and HTML Structure Implementing my own UIK-Framework Converting CSS to SCSS Subscribe Dialog Implemented In header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,137 +1,134 @@
 /* ----- Custom Scripts for Pilot template ----- */
 
-jQuery(function($) {
-  "use strict";
+jQuery(function ($) {
+	"use strict";
 
-  // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-    var mainbottom = $('#main').offset().top;
-    // on scroll,
-    $(window).on('scroll', function(){
-    // we round here to reduce a little workload
-    stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
-        $('.navbar').addClass('past-main');
-        $('.navbar').addClass('effect-main')
-    } else {
-        $('.navbar').removeClass('past-main');
-      }
-    });
+	// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+	var mainbottom = $('#main').offset().top;
+	// on scroll,
+	$(window).on('scroll', function () {
+		// we round here to reduce a little workload
+		stop = Math.round($(window).scrollTop());
+		if (stop > mainbottom) {
+			$('.navbar').addClass('past-main');
+			$('.navbar').addClass('effect-main')
+		} else {
+			$('.navbar').removeClass('past-main');
+		}
+	});
 
-  // Collapse navbar on click
+	// Collapse navbar on click
 
-  $(document).on('click.nav','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-    $(this).removeClass('in').addClass('collapse');
-   }
-  });
+	$(document).on('click.nav', '.navbar-collapse.in', function (e) {
+		if ($(e.target).is('a')) {
+			$(this).removeClass('in').addClass('collapse');
+		}
+	});
 
-/*----- Preloader ----- */
+	/*----- Preloader ----- */
 
-  $(window).load(function() {
-		setTimeout(function() {
-      $('#loading').fadeOut('slow', function() {
-      });
-    }, 300);
-  });
+	$(window).load(function () {
+		setTimeout(function () {
+			$('#loading').fadeOut('slow', function () {
+			});
+		}, 300);
+	});
 
-/* --------- Wow Init -------*/
+	/* --------- Wow Init -------*/
 
-  new WOW().init();
+	new WOW().init();
 
-/* ------- Magnific Popup ---------*/
+	/* ------- Magnific Popup ---------*/
 
-  $('.popup').magnificPopup({
-    disableOn: 0,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
+	$('.cusotmPopup').magnificPopup({
+		type: 'inline',
+		mainClass: 'mfp-fade',
+		preloader: true,
+		removalDelay: 300
+	});
 
-    fixedContentPos: false
-  });
+	/*-------- Owl Carousel ---------- */
 
-/*-------- Owl Carousel ---------- */
+	$(".reviews").owlCarousel({
 
-  $(".reviews").owlCarousel({
+		slideSpeed: 200,
+		items: 1,
+		singleItem: true,
+		autoPlay: true,
+		pagination: false
+	});
 
-    slideSpeed : 200,
-    items: 1,
-    singleItem: true,
-    autoPlay : true,
-    pagination : false
-  });
+	/* ------ Clients Section Owl Carousel ----- */
 
-/* ------ Clients Section Owl Carousel ----- */
-
-  $(".clients").owlCarousel({
-    slideSpeed : 200,
-    items: 5,
-    singleItem: false,
-    autoPlay : true,
-    pagination : false
-  });
+	$(".clients").owlCarousel({
+		slideSpeed: 200,
+		items: 5,
+		singleItem: false,
+		autoPlay: true,
+		pagination: false
+	});
 
 
-/*----------- Scroll To Top ---------------*/
+	/*----------- Scroll To Top ---------------*/
 
-  $(window).on('scroll', function () {
-    if ($(this).scrollTop() > 1000) {
-        $('#back-top').fadeIn();
-    } else {
-        $('#back-top').fadeOut();
-    }
-  });
-  // scroll body to 0px on click
-  $('#back-top').on('click', function () {
-    $('#back-top').tooltip('hide');
-    $('body,html').animate({
-        scrollTop: 0
-    }, 1500);
-    return false;
-  });
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 1000) {
+			$('#back-top').fadeIn();
+		} else {
+			$('#back-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('#back-top').on('click', function () {
+		$('#back-top').tooltip('hide');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1500);
+		return false;
+	});
 
-/* ------ Countdown ----- */
+	/* ------ Countdown ----- */
 
-  $('#countdown').countdown({
+	$('#countdown').countdown({
 		date: '12/07/2017 12:00:00',
 		offset: +2,
 		day: 'Day',
 		days: 'Days'
-	 }, function () {
+	}, function () {
 		// alert('Done!');
 	});
 
-/* ------ jQuery for Easing min -- */
+	/* ------ jQuery for Easing min -- */
 
-  $(function() {
-  $('a.page-scroll').on('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-    });
-  });
+	$(function () {
+		$('a.page-scroll').on('click', function (event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
 
 
-  /*----- Subscription Form ----- */
+	/*----- Subscription Form ----- */
 
-  $(document).ready(function() {
-    // jQuery Validation
-    $("#signup").validate({
-      // if valid, post data via AJAX
-      submitHandler: function(form) {
-        $.post("assets/php/subscribe.php", { email: $("#email").val() }, function(data) {
-          $('#response').html(data);
-        });
-      },
-      // all fields are required
-      rules: {
-        email: {
-          required: true,
-          email: true
-        }
-      }
-    });
-  });
-  });
+	$(document).ready(function () {
+		// jQuery Validation
+		$("#signup").validate({
+			// if valid, post data via AJAX
+			submitHandler: function (form) {
+				$.post("assets/php/subscribe.php", { email: $("#email").val() }, function (data) {
+					$('#response').html(data);
+				});
+			},
+			// all fields are required
+			rules: {
+				email: {
+					required: true,
+					email: true
+				}
+			}
+		});
+	});
+});
