2b30638da4aa4bad58abfe6e9f0a9bc430e60373
Farman Ahmed <farmanahmed2007@gmail.com>
2020-05-26 15:51:26 +0500

    updated

---
+++ beta/assets/js/custom.js
@@ -0,0 +1,29 @@
+/* ----- Custom Scripts for Pilot template ----- */
+
+jQuery(function ($) {
+	"use strict";
+
+	var radius = 8;
+    TweenMax.staggerFromTo('.blob', 4 ,{
+        cycle: {
+            attr:function(i) {
+                var r = i*90;
+                return {
+                    transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+                }
+            }
+        }
+    },{
+        cycle: {
+            attr:function(i) {
+                var r = i*90+360;
+                return {
+                    transform:'rotate('+r+') translate('+radius+',0.1) rotate('+(-r)+')'
+                }
+            }
+        },
+        ease:Linear.easeNone,
+        repeat:-1
+	});
+
+});
